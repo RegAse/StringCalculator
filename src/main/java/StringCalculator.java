2d93d4405ae492754bec44db4d145ea56de42b62
RegAse <brucejackie21@gmail.com>
2017-10-15 18:49:20 +0000

    Step 1: The calculator can add one number or more.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,16 @@
 public class StringCalculator {
+
+    public int Add(String numbers) {
+        int sum = 0;
+
+        if(numbers != "") {
+            String[] strnums = numbers.split(",");
+            for (int i = 0; i < strnums.length; i++) {
+                sum += Integer.parseInt(strnums[i]);
+            }
+        }
+
+        return sum;
+    }
+
 }
--- src/test/java/StringCalculatorTests.java
+++ src/test/java/StringCalculatorTests.java
@@ -1,2 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class StringCalculatorTests {
+
+    @Test
+    public void TestAddEmptyStringIsZero() {
+        StringCalculator sc = new StringCalculator();
+        String numbers = "";
+
+        int r = sc.Add(numbers);
+        Assert.assertEquals(0, r);
+    }
+
+    @Test
+    public void TestAddOneNumberString() {
+        StringCalculator sc = new StringCalculator();
+        String numbers = "1";
+
+        int r = sc.Add(numbers);
+        Assert.assertEquals(1, r);
+    }
+
+    @Test
+    public void TestAddTwoNumberString() {
+        StringCalculator sc = new StringCalculator();
+        String numbers = "1,2";
+
+        int r = sc.Add(numbers);
+        Assert.assertEquals(3, r);
+    }
+
+
 }
